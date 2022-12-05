74b9eb79d68c517b8005855ebba23cd6bd96bfe5
ramkumar46 <ramkumar4648@gmail.com>
2022-12-05 13:33:26 +0530

    Telegram

---
+++ src/test/java/telegram/TElegramLogin.java
@@ -0,0 +1,17 @@
+package telegram;
+
+public class TElegramLogin {
+
+private void loginuser() {
+	System.out.println("1");
+}
+private void pass() {
+	System.out.println("2");
+
+}
+private void login() {
+	System.out.println("3");
+
+}
+
+}
