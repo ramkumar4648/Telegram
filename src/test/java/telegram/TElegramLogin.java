99fd03ab0c3c60af1010ece9c49afd95fa8dc5ae
ramkumar46 <ramkumar4648@gmail.com>
2022-12-05 14:18:06 +0530

    Ram

--- src/test/java/telegram/TElegramLogin.java
+++ src/test/java/telegram/TElegramLogin.java
@@ -13,5 +13,13 @@ private void login() {
 	System.out.println("3");
 
 }
-
+private void method4() {
+	System.out.println("4");
+}
+private void method5() {
+	System.out.println("5");
+}
+private void method6() {
+	System.out.println("6");
+}
 }
